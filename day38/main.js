616e91b7c7d3a0c7b95d4d8c11a6688612ebf0e5
fabeas <bedirhan.ucbas@gmail.com>
2023-07-17 16:57:00 +0300

    .

--- day38/main.js
+++ day38/main.js
@@ -1,29 +1,21 @@
-const container = document.getElementById('container')
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 500
+const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
 
-for(let i = 0; i < SQUARES; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
+listItems.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
 
-    square.addEventListener('mouseover', () => setColor(square))
+        item.classList.add('active')
+        contents[idx].classList.add('show')
+    })
+})
 
-    square.addEventListener('mouseout', () => removeColor(square))
-
-    container.appendChild(square)
+function hideAllContents() {
+    contents.forEach(content => content.classList.remove('show'))
 }
 
-function setColor(element) {
-   const color = getRandomColor()
-   element.style.background = color
-   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
-}
 
-function removeColor(element) {
-   element.style.background = '#1d1d1d'
-   element.style.boxShadow = '0 0 2px #000'
-}
-
-function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)]
+function hideAllItems() {
+    listItems.forEach(item => item.classList.remove('active'))
 }
